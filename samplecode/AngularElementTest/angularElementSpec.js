a426bdcde6ced8ecdc6ba736edb707b1b7b5a96b
cjus <carlos.justiniano@gmail.com>
2014-06-13 09:43:56 -0700

    Cleanup

---
+++ samplecode/AngularElementTest/angularElementSpec.js
@@ -0,0 +1,16 @@
+describe('Angular element and expression test', function() {
+  var element
+    , $scope;
+
+  //beforeEach(module("myApp"));
+  beforeEach(inject(function($compile, $rootScope) {
+    $scope = $rootScope;
+    element = angular.element("<div>{{2 + 2}}");
+    element = $compile(element)($rootScope);
+    $scope.$digest();
+  }));
+
+  it('should equal 4', function() {
+    expect(element.html()).toBe("4");
+  });
+});
